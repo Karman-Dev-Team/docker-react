0a4510226ebfc0fd7b8e5294d94ac8b2dfa67920
Jaime Boardman <jaime@DESKTOP-J49I907.localdomain>
2022-06-27 16:07:01 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Tests
+<a href="https://app.hubspot.com/oauth/authorize?client_id=89b4588e-eac0-4521-aec0-13cd43fd19c1&redirect_uri=https://api.integratr.io/api/a7cee395-ae2f-4c24-bae3-e186c077f0a5&scope=crm.objects.contacts.read%20crm.objects.contacts.write%20crm.objects.companies.write%20crm.objects.companies.read%20crm.objects.deals.read">Link hubspot</a>
       </header>
     </div>
   );
